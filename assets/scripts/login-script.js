0c2d73fce9abc1e72aeb6b4594ec98d9ee801061
Uisss05 <131368540+Uisss05@users.noreply.github.com>
2023-08-28 13:51:55 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,31 +1,24 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-/* 
-variable setiap element view
-*/
+
+/* Comment : variable setiap element view*/
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 /*membuar variable untuk menyimpan informasi email dan password*/
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
-/*aksi klik pada button*/
+/*Comment : aksi klik pada button*/
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
  /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan.
-  /*memastikan bahwa email benar*/
+  /* Comment : memastikan bahwa email benar*/
   const email = inputEmailElement.value;
-  /*memastikan jika password benar*/
+  /*Comment : memastikan jika password benar*/
   const password = inputPasswordElement.value;
-  /*jika benar maka masuk kehalaman home*/
+  /*Comment : jika benar maka masuk kehalaman home*/
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
   }
-  /*jika salah maka muncul popup*/
+  /*Comment : jika salah maka muncul popup*/
   else {
     showPopUp();
   }
